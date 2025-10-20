b8d219366d05a021850a6d433b168fd3edf25663
yzx396 <jenson.zhao@gmail.com>
2025-10-20 14:15:47 -0700

    fix: remove UNIQUE constraint from google_id column in users table

--- migrations/0004_add_google_oauth.sql
+++ migrations/0004_add_google_oauth.sql
@@ -1,5 +1,5 @@
 -- Add Google OAuth support to users table
-ALTER TABLE users ADD COLUMN google_id TEXT UNIQUE;
+ALTER TABLE users ADD COLUMN google_id TEXT;
 
 -- Create index for faster lookups by google_id
 CREATE INDEX idx_users_google_id ON users(google_id);
